714e1449acedfc2326e6db9c32a629dec06ce072
HannahSu1 <hang-su@outlook.com>
2022-04-23 20:32:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from art import logo,vs
+import random
+from game_data import data
+from replit import clear
+
+
+def get_random_account():
+    """get random data from game data"""
+    return random.choice(data)
+    
+def format_data(account):
+    """format data for output"""
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    return f"{name}, a {description}, from {country}"
+
+def check_answer(guess,a_followers,b_followers):
+    """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong."""
+    if a_followers > b_followers:
+        return guess =="a"
+    else:
+        return guess =="b"
+
+def game():
+    print(logo)
+    score = 0
+    account_a = get_random_account()
+    account_b = get_random_account()
+    game_should_continue = True
+
+    while game_should_continue == True:
+        account_a = account_b
+        account_b = get_random_account()
+
+        while account_a == account_b:
+            account_b = get_random_account()
+        print(f"Compare A: {format_data(account_a)}.")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}.")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+        clear()
+        print(logo)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}")            
+
+game()
+
+# import the logo and and conditionA 
+# import the vs and conditionB
+
+# User to guess who has more followers
+
+# Define a function to hanlde result
+# If right, show answer is right and change conditionB to conditionA and compare with Condition B again
+# If wrong, show anser is wrong and how many right answer and stop game
